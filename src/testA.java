4be80c0b757ff05209fa36ed9b3eaa91ab06cace
LingjiaDeng <lingjiadeng@gmail.com>
2015-01-28 11:58:20 -0500

    1138

--- src/testA.java
+++ src/testA.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class testA {
 	public static void main(String[] args){
-		String s = "ccc";
+		String s = "1138";
 		System.out.println(s.substring(1, 3));
 	}
 
