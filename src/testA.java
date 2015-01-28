e53af08f455f6f56ce6a9d34b270a4f4d7ca2d4b
LingjiaDeng <lingjiadeng@gmail.com>
2015-01-28 11:35:34 -0500

    change to bbb

--- src/testA.java
+++ src/testA.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class testA {
 	public static void main(String[] args){
-		String s = "abcd";
+		String s = "bbbb";
 		System.out.println(s.substring(1, 3));
 	}
 
