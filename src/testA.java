2e8d1779a37227e9f933ddbc85e7762f5cd3b623
lid29 <lid29@pitt.edu>
2015-01-28 13:20:34 -0500

    from desktop

--- src/testA.java
+++ src/testA.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class testA {
 	public static void main(String[] args){
-		String s = "1224";
+		String s = "1319";
 		System.out.println(s.substring(1, 3));
 	}
 
