3ee49602c120dacdd1c4c31ea3ecfb6f42c9f010
LingjiaDeng <lingjiadeng@gmail.com>
2015-01-28 12:25:09 -0500

    1224

--- src/testA.java
+++ src/testA.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class testA {
 	public static void main(String[] args){
-		String s = "1205";
+		String s = "1224";
 		System.out.println(s.substring(1, 3));
 	}
 
